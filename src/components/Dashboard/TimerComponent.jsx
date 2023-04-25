b93f102c1e4430387a1390ed798a8d5fdf3a1730
Bhagwan Singh <bhagwan.5602@gmail.com>
2023-04-25 18:17:44 +0530

    timer component added

---
+++ src/components/Dashboard/TimerComponent.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import Box from '@mui/material/Box';
+
+import Swal from 'sweetalert2';
+
+
+const TimerComponent = () => {
+
+    // useEffect(() => {
+    //     Swal.fire({
+	// 		position: 'top-end',
+	// 		icon: 'success',
+	// 		title: 'Your work has been saved',
+	// 		showConfirmButton: false,
+	// 		timer: 120000
+	// 	  });
+    // })
+
+    return (
+        <>
+            <Box>
+                <h3>Hey.. few moments to change your choice.</h3>
+            </Box>
+        </>
+    )
+}
+export default TimerComponent;
